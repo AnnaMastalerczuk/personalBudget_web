d5bd891eb985e477288d3f48919e324ddc41a97e
AnnaMastalerczuk <anmastalerczuk@gmail.com>
2022-06-29 21:36:52 +0100

    registration form js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const inputPassword = document.querySelector('#password');
+// const inputPassword = document.querySelector('#password');
 const eyeBtn = document.querySelector('#visability');
 const eyeOffBtn = document.querySelector('#visavilityOff');
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+
 
 eyeBtn.addEventListener('click', function() {
-    inputPassword.type = 'text';
+    inputs[1].type = 'text';
     eyeBtn.classList.toggle('change');
     eyeOffBtn.classList.toggle('change');
 });
 
 eyeOffBtn.addEventListener('click', function() {
-    inputPassword.type = 'password';
+    inputs[1].type = 'password';
     eyeBtn.classList.toggle('change');
     eyeOffBtn.classList.toggle('change');
 });
+
+form.addEventListener('submit', () => {
+    const login = inputs[0].value;
+    const password = inputs[1].value;
+    const name = inputs[2].value;
+    const surname = inputs[3].value;
+})
 
