e93afb66897c45c92c5095c25bd4911d40654407
AnnaMastalerczuk <anmastalerczuk@gmail.com>
2022-06-30 19:31:02 +0100

    password visability js added improvement

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-// const inputPassword = document.querySelector('#password');
-const eyeBtn = document.querySelector('#visability');
-const eyeOffBtn = document.querySelector('#visavilityOff');
-const form = document.querySelector('form');
-const inputs = document.querySelectorAll('input');
-
+const inputPassword = document.querySelector('#password');
+const eyeBtn = document.querySelector('#togglePassword');
+let flag = true;
 
 eyeBtn.addEventListener('click', function() {
-    inputs[1].type = 'text';
-    eyeBtn.classList.toggle('change');
-    eyeOffBtn.classList.toggle('change');
+
+    if(flag){
+        inputPassword.type = 'text';
+        flag = false;
+    } else {
+        inputPassword.type = 'password';
+        flag = true;
+    }
+
+    eyeBtn.classList.toggle('bi-eye');
+
 });
-
-eyeOffBtn.addEventListener('click', function() {
-    inputs[1].type = 'password';
-    eyeBtn.classList.toggle('change');
-    eyeOffBtn.classList.toggle('change');
-});
-
-form.addEventListener('submit', () => {
-    const login = inputs[0].value;
-    const password = inputs[1].value;
-    const name = inputs[2].value;
-    const surname = inputs[3].value;
-})
-
